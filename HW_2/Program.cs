6d013b6b384eb465316283a6b8a9e4b0e7389156
Alex Nejen <alex_nas@Mac-mini-Alex.local>
2022-05-08 12:41:29 -0400

    modifited HW1
    Have done the second home work Max number out of 3

---
+++ HW_2/Program.cs
@@ -0,0 +1,20 @@
+﻿// Напишите программу, которая принимает на
+// вход три числа и выдаёт максимальное из этих чисел.
+// 2, 3, 7 -> 7
+// 44 5 78 -> 78
+// 22 3 9 -> 22
+Console.Write("Введите целое число А = ");
+int numA = int.Parse(Console.ReadLine());
+
+Console.Write("Введите целое число B = ");
+int numB = int.Parse(Console.ReadLine());
+
+Console.Write("Введите целое число C = ");
+int numC = int.Parse(Console.ReadLine());
+
+int max = numA;
+if (numA > max) max = numA;
+if (numB > max) max = numB;
+if (numC > max) max = numC;
+Console.Write("Наибольшее чистло из трех равно = ");
+Console.Write(max);
